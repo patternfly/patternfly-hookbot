c3cdbee4a49887da276596381d68ea37e091c6fe
zack allen <zallen@redhat.com>
2019-02-27 12:40:32 -0500

    bump hello

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.send('Hello world 4!');
+  res.send('Hello world 5!');
 });
 
 module.exports = router;
