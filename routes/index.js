19df087cbbcdd4cf5e5e64ed851b89037f2e9a9b
zack allen <zallen@redhat.com>
2019-02-27 15:31:20 -0500

    i don't need a Dockerfile?!

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.send('Hello world 5!');
+  res.send('Hello world 6!');
 });
 
 module.exports = router;
