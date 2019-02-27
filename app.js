aa00923bfb5a71efda3aaa451fbf1248fa8458b7
undefined <undefined>
2019-02-27 15:47:04 -0500

    change where git configs

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const path = require('path');
-const exec = require('child_process').execSync
 
 const indexRouter = require('./routes/index');
 const hooksRouter = require('./routes/hooks');
-
-exec(`git config --global user.email ${process.env.GIT_EMAIL}`);
-exec(`git config --global user.name ${process.env.GIT_USERNAME}`);
 
 const app = express();
 
