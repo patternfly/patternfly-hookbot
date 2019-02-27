076945c99a0cc4f8aa0efb8ca1bd91ddddda3dd0
zack allen <zallen@redhat.com>
2019-02-27 12:40:15 -0500

    log some more stuff

--- routes/hooks.js
+++ routes/hooks.js
@@ -9,10 +9,29 @@ router.get('/', function(req, res, next) {
     res.send(requests.toString());
 });
 
+function verifySecret(res) {
+    console.log('verifying secret', process.env.NPM_SECRET);
+    if ('' === process.env.NPM_SECRET)
+        return true;
+    
+    return false;
+}
+
 router.post('/npm', function(req, res) {
     requests.push(JSON.stringify(req.body));
-    console.log(req.body);
-    // upgradePackage('https://github.com/redallen/patternfly-react', '@patternfly/patternfly', '1.0.200')
+    console.log(req.headers);
+
+    verifySecret(res);
+
+    if (req.body['event'] === 'package:publish') {
+        if (req.body['name'] === '@patternfly/patternfly') {
+            upgradePackage('https://github.com/redallen/patternfly-react', req.body['name'], req.body['change']['version'])
+        }
+        else if (req.body['name'] === 'registrytestpackage') {
+            upgradePackage('https://github.com/redallen/patternfly-react', req.body['name'], req.body['change']['version'])
+        }
+    }
+    res.sendStatus(200);
 });
 
 module.exports = router;
