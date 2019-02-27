5575f584cbe19355d2bb84fd1a195cfbd3bea3ab
undefined <undefined>
2019-02-27 15:37:42 -0500

    no more git

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
+const exec = require('child_process').execSync
 
-var indexRouter = require('./routes/index');
-var hooksRouter = require('./routes/hooks');
+const indexRouter = require('./routes/index');
+const hooksRouter = require('./routes/hooks');
 
-var app = express();
+exec(`git config --global user.email ${process.env.GIT_EMAIL}`);
+exec(`git config --global user.name ${process.env.GIT_USERNAME}`);
+
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo 'entrypoint script'
-git config --global user.email ${GIT_EMAIL}
-git config --global user.name ${GIT_USERNAME}
-
-node bin/test.js
-npm start
